d3dd343aaabc6de76ef2f6dd84b2e1135e110f18
User1283859 <moritzfranke@hotmail.com>
2025-09-14 13:55:53 +0200

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -2,12 +2,18 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('my-cache').then(function(cache) {
       return cache.addAll([
-        '/',
+        '/app/',
+        '/app/index.html',
+        '/app/styles.css',
+        '/app/app.js',
+        '/app/icon512_maskable.png',
+        '/app/icon512_rounded.png',
         '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/icon512_maskable.png',
-        '/icon512_rounded.png'
+        '/netlify.toml',
+        '/package.json',
+        '/package-lock.json',
+        '/node_modules',
+        '/netlify'
       ]);
     })
   );
@@ -19,4 +25,5 @@ self.addEventListener('fetch', function(event) {
       return response || fetch(event.request);
     })
   );
+
 });
